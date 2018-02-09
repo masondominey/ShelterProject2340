4479dd867305f1f9dac83d0c64a25738806b67d8
kelseyjohnson <kelseyjohnson1919@gmail.com>
2018-02-09 14:18:02 -0500

    Created home screen with log in and register buttons

---
+++ app/src/main/java/comhwilco/github/shelterproject2340/MainActivity.java
@@ -0,0 +1,21 @@
+package comhwilco.github.shelterproject2340;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    // method called when user clicks login button
+
+    public void loginClick(View view){
+
+
+    }
+}
